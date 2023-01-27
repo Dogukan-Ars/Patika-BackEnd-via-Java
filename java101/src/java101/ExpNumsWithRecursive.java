baebd00fec8084d8cf3db07b944d9190dad5b909
Doğukan <arss.dogukan@gmail.com>
2023-01-27 17:02:37 +0300

    Fourth Commit

---
+++ java101/src/java101/ExpNumsWithRecursive.java
@@ -0,0 +1,24 @@
+package java101;
+
+import java.util.Scanner;
+
+public class ExpNumsWithRecursive {
+
+	static int exp(int a, int b) {
+		 if (a == 1 || b == 0) {	          
+	            return 1;
+	        }
+	        return a*exp(a,b-1);
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the base number : ");
+		int a = scan.nextInt();
+		System.out.print("Enter the exponential number : ");
+		int b = scan.nextInt();
+		
+		System.out.println(exp(a,b));
+	}
+
+}
